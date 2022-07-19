9610934f9ed799cb31a5fe2fef30f9b47c69ebdf
wyj41 <71953208+wyj41@users.noreply.github.com>
2022-07-19 21:48:52 +0800

    Delete HospConfig.java

--- model/src/main/java/com/myproject/yygh/config/HospConfig.java
+++
@@ -1,9 +0,0 @@
-package com.myproject.yygh.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.myproject.yygh.hosp.mapper")
-public class HospConfig {
-}
